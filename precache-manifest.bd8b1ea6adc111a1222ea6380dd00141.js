bc4958be74a602a357e42651efbec59f0a6ca582
KAMAN097 <63709039+KAMAN097@users.noreply.github.com>
2020-05-30 11:53:01 +0530

    Updates

---
+++ precache-manifest.bd8b1ea6adc111a1222ea6380dd00141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b6e8eae7d5146c4e0d8af093b0268f",
+    "url": "/funky/index.html"
+  },
+  {
+    "revision": "a4fe4eac96e5419d3e79",
+    "url": "/funky/static/css/main.79dc88d5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/funky/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funky/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fe4eac96e5419d3e79",
+    "url": "/funky/static/js/main.3c908562.chunk.js"
+  },
+  {
+    "revision": "ad315a210532a1d67383",
+    "url": "/funky/static/js/runtime-main.b9b5a7cb.js"
+  }
+]);
